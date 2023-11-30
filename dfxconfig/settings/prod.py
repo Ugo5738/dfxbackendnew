9f0f18ac16b234dc7e3cc5adcc70321bacf4ebc7
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2023-11-30 21:24:21 +0100

    remove redis url

--- dfxconfig/settings/prod.py
+++ dfxconfig/settings/prod.py
@@ -59,15 +59,15 @@ DEFAULT_FROM_EMAIL = config("EMAIL")
 
 
 # ================================ REDIS =======================================
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": config("REDIS_URL"),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": config("REDIS_URL"),
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 # ================================ REDIS =======================================
 
 
