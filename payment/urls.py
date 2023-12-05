8c0c84fbe87b7db3b6419772f638eb0daf40d153
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2023-12-05 12:13:39 +0100

    removed Test api

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,4 @@ from payment import views
 
 urlpatterns = [
     path('initiate-payment/', views.InitiatePayment.as_view(), name='initiate_payment'),
-    path('test/', views.Test.as_view(), name='test'),
 ]
